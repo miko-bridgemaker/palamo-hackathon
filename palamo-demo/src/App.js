705b13f945e91905a282bd7d1b3a246b27aa91ec
Adam Szlag <adamszlag@MacBook-Pro-Adam.local>
2021-08-24 11:35:29 +0200

    feat/scene-component

--- palamo-demo/src/App.js
+++ palamo-demo/src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import './components/scene'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         test22
+        <scene />
       </header>
     </div>
   );
