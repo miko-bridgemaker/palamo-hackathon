fd0633949e539a3124242ffc6aebfd90cac36db2
Adam Szlag <adamszlag@MacBook-Pro-Adam.local>
2021-08-24 16:48:37 +0200

    image and animations

--- palamo-demo/src/components/Scene/Scene.js
+++ palamo-demo/src/components/Scene/Scene.js
@@ -5,6 +5,7 @@ const Scene = () => {
     const [canvasHeight, setCanvasHeight] = useState(200);
     const [canvasRadius, setCanvasRadius] = useState(0);
     const [material, setMaterial] = useState();
+    const [varnish, setVarnish] = useState();
     const handleCanvas = () =>{
         document.getElementById('canvas').style.width = `${canvasWidth}px`;
         document.getElementById('canvas').style.height = `${canvasHeight}px`;
@@ -22,6 +23,19 @@ const Scene = () => {
         document.getElementById('material').style.background = `${material}`;
       }
 
+      const handleVarnish = () => {
+          if (varnish==0){
+            var img = document.querySelector('#myImg');
+            img.classList.remove('matte');
+            img.classList.add('shiny');
+          }
+          else {
+            var img = document.querySelector('#myImg');
+            img.classList.remove('shiny');
+            img.classList.add('matte');
+          }
+      }
+
       const roundCanvas = (x) => {
         setCanvasRadius(x);
       }
@@ -30,12 +44,30 @@ const Scene = () => {
         setMaterial(x);
       }
 
+      const finishing = (x) => {
+          setVarnish(x);
+      }
+
+      window.addEventListener('load', function() {
+        document.querySelector('input[type="file"]').addEventListener('change', function() {
+            if (this.files && this.files[0]) {
+                var img = document.querySelector('#myImg');
+                img.style.visibility = 'visible';
+                handleCanvas();
+                img.onload = () => {
+                    URL.revokeObjectURL(img.src);  // no longer needed, free memory
+                }
+      
+                img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+            }
+        });
+      });
     
     return (
         <div className='app-scene'>
             <div className='canvas' id='canvas'></div>
             <div className='background-material' id='material'></div>
-            <div className='user-img' id='label'></div>
+            <div className='user-img' id='label'><img id="myImg" src="#"></img></div>
             {canvasHeight}px/{canvasWidth}px
             <button onClick={() => setCanvasHeight(canvasHeight+100)}>+ height</button>
             <button onClick={() => setCanvasWidth(canvasWidth+100)}>+ width</button>
@@ -48,10 +80,13 @@ const Scene = () => {
             <button onClick={() => setBgMaterial('blue')}>Blue material</button>
             <button onClick={() => setBgMaterial('red')}>Red material</button>
 
-            <button onClick={() => handleMaterial()}>Update material</button>
+            <button onClick={() => (handleMaterial(), handleCanvas())}>Update material</button>
             <br></br>
-            
-            
+            <input type='file' />
+            <br />
+            <button onClick={() => finishing(0)}>Make it shiny</button>
+            <button onClick={() => finishing(1)}>Make it matte</button>
+            <button onClick={() => (handleMaterial(), handleCanvas(), handleVarnish())}>Update varnish</button>
         </div>
     )
 }
