dc7808b7c9caaf387d6f72d0fd4a6db7871bce45
Adam Szlag <adamszlag@MacBook-Pro-Adam.local>
2021-08-24 15:41:01 +0200

    minor scane changes

--- palamo-demo/src/components/Scene/Scene.js
+++ palamo-demo/src/components/Scene/Scene.js
@@ -4,6 +4,7 @@ const Scene = () => {
     const [canvasWidth, setCanvasWidth] = useState(200);
     const [canvasHeight, setCanvasHeight] = useState(200);
     const [canvasRadius, setCanvasRadius] = useState(0);
+    const [material, setMaterial] = useState();
     const handleCanvas = () =>{
         document.getElementById('canvas').style.width = `${canvasWidth}px`;
         document.getElementById('canvas').style.height = `${canvasHeight}px`;
@@ -17,9 +18,30 @@ const Scene = () => {
 
       };
 
+      const handleMaterial = () =>{
+        document.getElementById('material').style.background = `${material}`;
+      }
+
       const roundCanvas = (x) => {
         setCanvasRadius(x);
       }
+
+      const setBgMaterial = (x) => {
+        setMaterial(x);
+      }
+
+      window.addEventListener('load', function() {
+        document.querySelector('input[type="file"]').addEventListener('change', function() {
+            if (this.files && this.files[0]) {
+                var img = document.querySelector('img');
+                img.onload = () => {
+                    URL.revokeObjectURL(img.src);  // no longer needed, free memory
+                }
+      
+                img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+            }
+        });
+      });
 
     return (
         <div className='app-scene'>
@@ -31,7 +53,16 @@ const Scene = () => {
             <button onClick={() => setCanvasWidth(canvasWidth+100)}>+ width</button>
             <button onClick={() => roundCanvas(9999)}>Make it round</button>
             <button onClick={() => roundCanvas(0)}>Make it square</button>
+
             <button onClick={() => handleCanvas()}>Update canvas</button>
+            <br></br>
+
+            <button onClick={() => setBgMaterial('blue')}>Blue material</button>
+            <button onClick={() => setBgMaterial('red')}>Red material</button>
+
+            <button onClick={() => handleMaterial()}>Update material</button>
+            <br></br>
+            
             
         </div>
     )
